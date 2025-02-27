3711139a8b5c829bdc913e23032d2ce81d146f15
Kaf <coffeechopdev@gmail.com>
2025-02-27 11:18:37 -0300

    feat: add event system feat: add platform helpers feat: add json file reader feat: remove architectury dependency chore: command and quilt stuff has been removed

--- changelog.md
+++ changelog.md
@@ -1,6 +1,21 @@
 # Amber Changelog
 
-See the full changelog at ...
+See the full changelog at https://github.com/iamkaf/amber/commits
+
+## 3.0.0
+
+- feat: add event system
+- feat: add platform helpers
+- feat: add json file reader
+- feat: remove architectury dependency
+
+## 2.1.0
+
+- add a few more inventory methods
+
+## 2.0.0-beta.3
+
+- port to 1.21.4
 
 ## 1.3.0-beta.2
 
---
+++ fabric/src/main/java/com/iamkaf/amber/mixin/LivingEntityMixin.java
@@ -0,0 +1,33 @@
+package com.iamkaf.amber.mixin;
+
+import com.iamkaf.amber.api.event.v1.events.common.EntityEvent;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+    @Shadow
+    protected float lastHurt;
+
+    @Shadow
+    public abstract boolean isDeadOrDying();
+
+    @Shadow
+    public abstract boolean isDamageSourceBlocked(DamageSource damageSource);
+
+    @Inject(method = "hurt", at = @At("TAIL"))
+    private void amber$afterDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir, @Local(ordinal
+            = 1) float f) {
+        if (!isDeadOrDying()) {
+            EntityEvent.AFTER_DAMAGE.invoker()
+                    .afterDamage((LivingEntity) (Object) this, source, f, amount, this.isDamageSourceBlocked(source));
+        }
+    }
+}
