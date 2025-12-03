8342f68512fe9a36ca1f0be2656f25180abb687d
Kaf <coffeechopdev@gmail.com>
2025-12-03 11:25:09 -0300

    chore(docs): update copyright year to 2025

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -85,7 +85,7 @@ export default defineConfig({
 
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2024 iamkaf'
+      copyright: 'Copyright © 2025 iamkaf'
     }
   },
 
